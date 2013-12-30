6c812333b934b9c71b2d235d4ffd72b1897fd65a
Luke A <lukasz.adamczewski@polcode.pl>
2013-12-30 19:20:45 +0100

    Some fix for modal

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2ff161ba9b37e7287eb5a192b7b6399b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cde8b82e403cfd927659c3214e7938cb');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
