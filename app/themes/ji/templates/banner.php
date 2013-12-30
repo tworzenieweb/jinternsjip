ac29ed9fc91136ba0d9736428dc0793b015fdb33
Luke A <lukasz.adamczewski@polcode.pl>
2013-12-30 19:15:56 +0100

    Fix for modal

--- app/themes/ji/templates/banner.php
+++ app/themes/ji/templates/banner.php
@@ -45,7 +45,10 @@
         </div>
     </div>
 
-    <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
+</div>
+
+<div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-body">
@@ -54,5 +57,3 @@
             </div>
         </div>
     </div>
-
-</div>
