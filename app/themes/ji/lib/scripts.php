1c987d5561284b320caf871d0e4554b15b386480
Luke A <lukasz.adamczewski@polcode.pl>
2013-12-31 16:16:24 +0100

    Added responsive fix

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a55bf0685b3606063c34232f2c93a9c9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6b5d39bff0c43a05d1b73f6878b219e7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
