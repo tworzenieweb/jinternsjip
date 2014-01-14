6d1a8982e8cab7a6b43a0807dca98db56c2fb342
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-14 15:22:26 +0100

    Added stylings for sessions

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ee901e511310920a835520a9eef71997');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e8ec043b28f19f7437ae0ec7a6a4b4f0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
