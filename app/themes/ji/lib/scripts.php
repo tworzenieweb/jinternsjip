3de1d8d1322a9b6ea79c2afbfad7790ae0509b7a
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-14 11:14:13 +0100

    Added styling for testiminials

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ec407d563733f81319b7ae0f1ab517d9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6152decadf7a0e9c98ed3439f73326df');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
