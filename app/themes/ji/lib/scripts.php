adc48e466dda6d94f9382b40adcec7ad6a490335
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-15 18:26:11 +0100

    Added styling for faq

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '05e0b7c49ba04d50e13b524ef501f781');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e594da8979d4539dce822067341db50e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
