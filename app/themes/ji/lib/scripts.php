3c7b0267069285120ba5d00f54b0113cbfdc4b0e
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-15 18:31:40 +0100

    Some fix

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e594da8979d4539dce822067341db50e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bf9d8dc74ce6c8366c838870317ef957');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
