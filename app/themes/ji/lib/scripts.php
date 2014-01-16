fad867d0e7caceaaf7e49fe519ed9363723c2001
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-16 19:18:48 +0100

    Added frontend

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '72ff7150f9b5eae41689a753fd5be825');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '086ab5894f362ec11ca6c9a881dd601a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
