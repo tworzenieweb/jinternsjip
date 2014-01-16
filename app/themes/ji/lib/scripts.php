87475144397ba0a6f8cb96d2eddd9b732546b592
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-16 11:53:40 +0100

    Added assets faq

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bf9d8dc74ce6c8366c838870317ef957');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '69ac9d1747a3b66e6500fd17956a15b8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
