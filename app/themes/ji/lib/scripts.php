f23cf258911068a0843f30322d65104742decbe3
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-17 14:26:56 +0100

    Added assets for jewish learning program

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '086ab5894f362ec11ca6c9a881dd601a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e66224745eb35b3d7f762fdf6d74d09c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
