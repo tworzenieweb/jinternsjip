d8c8b1cc9f8c9503c8ab6ad721fcc9847fdee528
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-19 23:32:14 +0100

    Fixed font

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '32f98c2a7e76e8530cea64222737b49f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '128c1abbfb888b252f7448c1fb3db88e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
