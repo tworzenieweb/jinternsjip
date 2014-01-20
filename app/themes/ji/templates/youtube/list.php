69c2df52da2ae3dbbc6b78c5dea829dd16a7abe7
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-20 18:33:51 +0100

    Fixed globals

--- app/themes/ji/templates/youtube/list.php
+++ app/themes/ji/templates/youtube/list.php
@@ -1,12 +1,11 @@
-<?php global $post; ?>
 <?php if($loop->have_posts()): ?>
 <div class="row">
     <div class="xs-col-10">
         <?php $loop->the_post(); ?>
         
         <iframe id="ytplayer" type="text/html" width="100%" height="345"
-  src="http://www.youtube.com/embed/<?php echo get_post_meta($post->ID, "_youtube_id", true); ?>"
-  frameborder="0"/>
+                src="http://www.youtube.com/embed/<?php echo get_post_meta(get_the_ID(), "_youtube_id", true); ?>"
+                frameborder="0"/>
         
     </div>
     <div class="xs-col-2">
@@ -14,7 +13,7 @@
 <?php while ($loop->have_posts()): $loop->the_post(); ?>
 
             <li>
-                <img src="http://img.youtube.com/vi/<?php echo get_post_meta($post->ID, "_youtube_id", true); ?>/1.jpg"
+                <img src="http://img.youtube.com/vi/<?php echo get_post_meta(the_ID(), "_youtube_id", true); ?>/1.jpg"
             </li>
 
 <?php endwhile; ?>
