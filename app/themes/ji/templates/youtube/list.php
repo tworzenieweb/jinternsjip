70be31ff37c9f3a17ee6bab0fad1e70dcf45c66e
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-20 18:49:52 +0100

    some fix

--- app/themes/ji/templates/youtube/list.php
+++ app/themes/ji/templates/youtube/list.php
@@ -2,11 +2,7 @@
 <div class="row">
     <div class="xs-col-10">
         <?php $loop->the_post(); ?>
-        
-        <iframe id="ytplayer" type="text/html" width="100%" height="345"
-                src="http://www.youtube.com/embed/<?php echo get_post_meta(get_the_ID(), "_youtube_id", true); ?>"
-                frameborder="0" />
-        
+        <iframe id="ytplayer" type="text/html" width="100%" height="345" src="http://www.youtube.com/embed/<?php echo get_post_meta(get_the_ID(), "_youtube_id", true); ?>" frameborder="0"></iframe>
     </div>
     <div class="xs-col-2">
         <ul class="list-unstyled thumbnails-youtube">
