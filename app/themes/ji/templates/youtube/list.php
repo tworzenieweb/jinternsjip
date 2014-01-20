33a7d5fa3f7856f642dba2ceabcf1d3e1a4ec6f2
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-20 18:22:11 +0100

    Added youtube

---
+++ app/themes/ji/templates/youtube/list.php
@@ -0,0 +1,23 @@
+<?php if($loop->have_posts()): ?>
+<div class="row">
+    <div class="xs-col-10">
+        <?php $loop->the_post(); ?>
+        
+        <iframe id="ytplayer" type="text/html" width="100%" height="345"
+  src="http://www.youtube.com/embed/<?php echo get_post_meta($post->ID, "_youtube_id", true); ?>"
+  frameborder="0"/>
+        
+    </div>
+    <div class="xs-col-2">
+        <ul class="list-unstyled thumbnails-youtube">
+<?php while ($loop->have_posts()): $loop->the_post(); ?>
+
+            <li>
+                <img src="http://img.youtube.com/vi/<?php echo get_post_meta($post->ID, "_youtube_id", true); ?>/1.jpg"
+            </li>
+
+<?php endwhile; ?>
+        </ul>
+    </div>
+</div>
+<?php endif; ?>
