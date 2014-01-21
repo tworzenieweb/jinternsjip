de22903bb5a246dfb8a357a838683351b4c55946
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-21 16:26:25 +0100

    some fixes for initial styles

--- app/themes/ji/lib/config.php
+++ app/themes/ji/lib/config.php
@@ -56,7 +56,8 @@ function roots_display_sidebar() {
     array(
       'is_404',
       'is_front_page',
-      'is_page'
+      'is_page',
+      'is_single',
     ),
     /**
      * Page template checks (via is_page_template())
