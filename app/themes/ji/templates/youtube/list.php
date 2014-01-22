5fa515f7c82a513a66542083db6a6db11cd3fa61
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-22 11:40:32 +0100

    fixed form stylings

--- app/themes/ji/templates/youtube/list.php
+++ app/themes/ji/templates/youtube/list.php
@@ -10,7 +10,7 @@
         <ul class="list-unstyled thumbnails-youtube">
             <?php while ($loop->have_posts()): $loop->the_post(); ?>
                 <li>
-                    <a href="http://www.youtube.com/embed/<?php echo get_post_meta(get_the_ID(), "_youtube_id", true); ?>" class="change-movie" title="<?php the_title(); ?>" data-toggle="tooltip">
+                    <a href="http://www.youtube.com/embed/<?php echo get_post_meta(get_the_ID(), "_youtube_id", true); ?>&amp;autoplay=0&amp;cc_load_policy=1&amp;hd=1&amp;controls=1&amp;autohide=1&amp;rel=0&amp;modestbranding=1&amp;showinfo=0&amp;wmode=opaque&amp;html5=1" class="change-movie" title="<?php the_title(); ?>" data-toggle="tooltip">
                         <img src="http://img.youtube.com/vi/<?php echo get_post_meta(get_the_ID(), "_youtube_id", true); ?>/1.jpg" />
                     </a>
                 </li>
