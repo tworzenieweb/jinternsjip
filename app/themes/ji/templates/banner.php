3c73a45611a5bd0864efde3959102d0f114a5227
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-22 13:51:13 +0100

    Added assets

--- app/themes/ji/templates/banner.php
+++ app/themes/ji/templates/banner.php
@@ -2,20 +2,18 @@
 
     <div class="container">
 
-
-
         <div class="jumbotron">
             <div id="sequence">
                 <a href="#" class="sequence-prev">Previous</a>
                 <a href="#" class="sequence-next">Next</a>
                 <ul class="sequence-canvas">       
                     <?php
-                    $args = array('post_type' => 'slide', 'posts_per_page' => 5);
-                    $loop = new WP_Query($args);
-                    $images = $posts = array();
-                    while ($loop->have_posts()) : $loop->the_post();
+                    $bannerArgs = array('post_type' => 'slide', 'posts_per_page' => 5);
+                    $loopBaner = new WP_Query($bannerArgs);
+                    $images = $postsCollection = array();
+                    while ($loopBaner->have_posts()) : $loopBaner->the_post();
                         ?>
-                        <?php $posts[] = $post; ?>
+                        <?php $postsCollection[] = $post; ?>
                         <?php $url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full', true); ?>
                         <li data-youtube="<?php echo get_post_meta($post->ID, "_youtube", true); ?>" data-background="<?php echo $url[0] ?>">
                             <div class="start_anime">
@@ -29,10 +27,11 @@
                             </div>
                         </li>
                     <?php endwhile; ?>
+                    <?php wp_reset_query(); ?>
 
                 </ul>
                 <ul class="sequence-pagination">
-                    <?php foreach ($posts as $key => $post): ?>
+                    <?php foreach ($postsCollection as $key => $post): ?>
                         <li>
                             <?php $url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail', true); ?>
 
