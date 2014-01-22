10e387d3bb0cb69b5da5e57ab8ae1a2194d2be3d
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-22 11:24:45 +0100

    fixed form stylings

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bcb91e665037a99233e2a8302749341d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5384b1d60062079bea2bdba05e3ca6a9');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
