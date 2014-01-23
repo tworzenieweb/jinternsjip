b8e32d3d2ea231a1b09ae3e903852974054ce275
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-23 13:22:44 +0100

    changes from spec

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9cd7c2be807a8be5a421a3d379c0f575');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '62484bff4598545ef149d71060bd3561');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
