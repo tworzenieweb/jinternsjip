fb1a116ddb350549f043577b868c7d6da5219178
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-29 15:52:36 +0100

    added like plugin

--- app/themes/ji/templates/content-page.php
+++ app/themes/ji/templates/content-page.php
@@ -2,3 +2,4 @@
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
+<div class="fb-like" data-href="https://www.facebook.com/JInternship" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
