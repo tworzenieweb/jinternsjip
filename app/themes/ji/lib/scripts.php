b7acd35ca3512d0690ccb2e1784f84518fd18805
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-29 18:26:28 +0100

    moved facebook block

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3046f5605b6068868352f9cd7866293f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a3dadc0210bd22db865d84135b7938a6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
