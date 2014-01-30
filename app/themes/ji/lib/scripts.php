24bdab4fd7cd235ad1c9cf9ac218d7bff26252bb
Luke A <lukasz.adamczewski@polcode.pl>
2014-01-30 16:12:20 +0100

    homepage fix

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '58242ccd2dfb1e28eec0741f4e292f2b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ddecdfd6bd3489d364c58d4561178d29');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
