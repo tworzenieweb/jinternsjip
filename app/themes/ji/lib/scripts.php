122bc4fad3b663a08394b8c65e9f6f9b199c058f
Luke A <lukasz.adamczewski@polcode.pl>
2014-03-13 15:22:54 +0100

    fixed height for frontpages

--- app/themes/ji/lib/scripts.php
+++ app/themes/ji/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8d5d5edf8488cce2581171a655586e9a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '43af12f8fad3f3c3c91cfcddfd616c1a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
